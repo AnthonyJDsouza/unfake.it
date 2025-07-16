26f82a4b407deb13a9e062126269488e1f162ccf
tororo.in <aj.anthonydsouza@gmail.com>
2025-07-16 06:54:07 +0200

    created train.py

---
+++ scripts/train.py
@@ -0,0 +1,48 @@
+from src.loss import FocalLoss
+from src.model import ImageClassifier
+from src.data import prepare_dataloaders
+from lightning import Trainer
+from lightning.pytorch.loggers import WandbLogger
+import wandb
+from datasets import load_from_disk, concatenate_datasets
+import argparse
+MODEL_NAME =    
+parser = argparse.ArgumentParser()
+parser.add_argument('--ds0', type=str, required=True)
+parser.add_argument('--ds1', type=str, required=True)
+parser.add_argument('--ds2', type=str, required=True)
+args = parser.parse_args()
+
+
+ds0 = load_from_disk(args.ds0)
+ds1 = load_from_disk(args.ds1)
+ds2 = load_from_disk(args.ds2)
+
+ds = concatenate_datasets([ds1, ds2])
+
+train_dl, val_dl, num_labels = prepare_dataloaders(
+    train_dataset=ds,
+    val_dataset=ds0,
+    model_name=MODEL_NAME
+)
+
+loss_fn = FocalLoss(gamma=0.8, alpha=0.75)
+
+model = ImageClassifier(
+    model_name=MODEL_NAME,
+    num_labels=num_labels,
+    loss_fn=loss_fn
+)
+
+trainer = Trainer(
+    max_epochs=1,
+    accelerator='auto',
+    precision='bf16-mixed',
+    logger=WandbLogger
+)
+
+trainer.fit(
+    train_dataloaders=train_dl,
+    val_dataloaders=val_dl,
+)
+
