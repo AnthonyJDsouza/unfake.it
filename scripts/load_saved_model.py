752e30c3dae79d278986d8b24d86504348f9af53
ajdsouza <aj.anthonydsouza@gmail.com>
2025-07-22 21:39:01 +0200

    sanity checks for model weights

--- scripts/load_saved_model.py
+++ scripts/load_saved_model.py
@@ -3,10 +3,32 @@ import torch
 import torch.nn as nn
 import lightning as L
 from peft import PeftModel
+from dotenv import load_dotenv
+import gc
+import psutil
+from transformers import AutoFeatureExtractor
 
+load_dotenv()
+
+token = os.getenv("HUGGINGFACE_HUB")
 model_path = ''
 
 model = ImageClassifier.load_from_checkpoint(model_path)
 peft_model = model.lora_model
+
+print(type(peft_model))
+
+total_params = sum(p.numel() for p in peft_model.parameters())
+print(f"Total parameters: {total_params}")
+
+gc.collect()
+print(f"RAM used: {psutil.Process().memory_info().rss / 1e9:.2f} GB")
+
+ae = AutoFeatureExtractor.from_pretrained("google/vit-base-patch32-224-in21k")
+dummy_input = ae(images=[torch.rand(3, 224, 224)], return_tensors="pt")
+
+with torch.no_grad():
+    outputs = peft_model(**dummy_input)
+print(outputs.last_hidden_state.shape)
 
 peft_model.save_pretrained('/scratch/ajdsouza/models/dfdc/vit_lora_adapter')
